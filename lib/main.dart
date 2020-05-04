3f3edd01825dfbd4e46aa96166b2aed45fadb405
aggarwalnaman <aggarwalnaman78@gmail.com>
2020-05-05 01:25:33 +0530

    Switch theme feature added

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 import 'quote.dart';
 import 'quote_card.dart';
 
+Brightness theme = Brightness.light;
+bool themeLight = true;
+
 void main() => runApp(MaterialApp(
+  title: "Quotes",
   home: quoteList(),
+  theme: ThemeData(
+    primarySwatch: Colors.green,
+    brightness: theme,
+    accentColor: Colors.blue,
+  ),
 ));
 
 class quoteList extends StatefulWidget {
@@ -18,7 +27,7 @@ class _quoteListState extends State<quoteList> {
     Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius'),
     Quote(author: 'Oscar Wilde', text: 'I can resist everything except temptation.'),
     Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple'),
-    Quote(author: 'Naman Aggarwal', text: 'Talk is cheap. Show me the code.'),
+    // Quote(author: 'Naman Aggarwal', text: 'Talk is cheap. Show me the code.'),
   ];
 
   @override
@@ -28,8 +37,30 @@ class _quoteListState extends State<quoteList> {
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text('Awesome Quotes'),
-        centerTitle: true,
         backgroundColor: Colors.red,
+        actions: <Widget>[
+          FlatButton.icon(
+            onPressed: () {
+              if (themeLight) {
+                themeLight = false;
+                setState(() {
+                  theme = Brightness.light;
+                  main();
+                });   
+              }
+              else
+              {
+                themeLight = true;
+                setState(() {
+                  theme = Brightness.dark;
+                  main();
+                });
+              }
+            },
+             icon: Icon(Icons.wb_sunny),
+             label: Text("Switch Theme"),
+            )
+        ],
       ),
       body: Column(
         children: quotes.map((quote) => QuoteCard(
